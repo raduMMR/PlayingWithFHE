1d2e2e07712ae391e568a7e68b3443437770827c
raduMMR <radu.mardari12@gmail.com>
2018-03-27 17:30:22 +0000

    rezolvat bug nr negative - must init carry

--- test2.cpp
+++ test2.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-	if(argc != 3)
+if(argc != 3)
     {
         cout<<"Nr de parametri incorect.\n";
         return -1;
@@ -18,7 +18,7 @@ int main(int argc, char **argv) {
     int8_t plaintext2 = atoi(argv[2]);
 
     cout<<"Sa batem HElib-ul !!!!"<<endl;
-	cout<<"Generare parametri si cheii ..."<<endl;
+	cout<<"Generare parametri si chei ..."<<endl;
 	const int minimum_lambda  = 110;
 	TFheGateBootstrappingParameterSet* params = new_default_gate_bootstrapping_parameters(minimum_lambda);
 	uint32_t seed[] = {314, 1592, 657};
@@ -29,19 +29,19 @@ int main(int argc, char **argv) {
 	cout<<"Criptare numere ...\n";
 	LweSample* ca = new_gate_bootstrapping_ciphertext_array(N, params);
 	for (int i=0; i<N; i++) {
-	// cout<<i<<" "<<((plaintext1>>i)&1)<<" ";
-        bootsSymEncrypt(&ca[i], (plaintext1>>i)&1, key);
-    }
+		bootsSymEncrypt(&ca[i], (plaintext1>>(N-1-i))&1, key);
+   	}
 	LweSample* cb = new_gate_bootstrapping_ciphertext_array(N, params);
 	for (int i=0; i<N; i++) {
-        bootsSymEncrypt(&cb[i], (plaintext2>>i)&1, key);
-    }
-	cout<<"Terminate de criptat numere.\n";
+        	bootsSymEncrypt(&cb[i], (plaintext2>>(N-1-i))&1, key);
+    	}
+	cout<<"Terminat de criptat numere.\n";
 
 	cout<<"Inmultire numere ...\n";
 	LweSample* result = new_gate_bootstrapping_ciphertext_array(N*2, params);
     LweSample* nr1 = new_gate_bootstrapping_ciphertext_array(N*2, params);
     LweSample* nr2 = new_gate_bootstrapping_ciphertext_array(N*2, params);
+	clock_t begin = clock();
     for(int i=0; i<N; i++){
         bootsCOPY(&nr1[i], &ca[0], &key->cloud);
         bootsCOPY(&nr2[i], &cb[0], &key->cloud);
@@ -57,41 +57,61 @@ int main(int argc, char **argv) {
     int pas=0;
     LweSample* bit_product = new_gate_bootstrapping_ciphertext(params);
     LweSample* carry = new_gate_bootstrapping_ciphertext(params);
-    bootsCONSTANT(carry, 0, &key->cloud);
     LweSample* aux = new_gate_bootstrapping_ciphertext(params);
     LweSample* prev = new_gate_bootstrapping_ciphertext(params);
 
     for(int i=0; i<2*N; i++){
+	    bootsCONSTANT(carry, 0, &key->cloud);
+
         for(int j=0; j<2*N-pas; j++){
             bootsCOPY(prev, &result[2*N-1-j-pas], &key->cloud);
 
             bootsAND(bit_product, &nr1[2*N-1-i], &nr2[2*N-1-j], &key->cloud);
+	   //  cout<<"bit="<<bootsSymDecrypt(bit_product, key)<<endl;
+	/*if(i==2){
+		cout<<"bit="<<bootsSymDecrypt(bit_product, key)<<endl;
+		cout<<"result["<<(2*N-1-j-pas)<<"] = "<<bootsSymDecrypt(&result[2*N-1-j-pas], key)<<endl;
+	}*/
             bootsXOR(aux, carry, bit_product, &key->cloud);
             bootsXOR(&result[2*N-1-j-pas], aux, &result[2*N-1-j-pas], &key->cloud);
 
+	/*if(i==2){
+		cout<<"result="<<bootsSymDecrypt(&result[2*N-1-j-pas], key)<<endl;
+	}*/	
             bootsAND(aux, bit_product, carry, &key->cloud);
             bootsAND(bit_product, bit_product, prev, &key->cloud);
             bootsOR(aux, aux, bit_product, &key->cloud);
             bootsAND(carry, carry, prev, &key->cloud);
             bootsOR(carry, aux, carry, &key->cloud);
+
         }
+	/*for(int j=0; j<2*N; j++){
+	    cout<<bootsSymDecrypt(&result[j], key);
+	}
+	cout<<endl;*/
         pas++;
     }
-	cout<<"Terminat de inmultit numerele.\n";
-
-	int16_t int_answer = 0;
-    for (int i=0; i<16; i++) {
-        int ai = bootsSymDecrypt(&result[i], key);
-        int_answer |= (ai<<(16-i));
-    }
-	cout<<"Produsul este "<<int_answer<<endl;
-
-	/*clock_t begin = clock();
 	clock_t end = clock();
 	double elapsed_secs = double(end-begin)/CLOCKS_PER_SEC;
-	cout<<"Timp = "<<elapsed_secs<<" secunde.\n";*/
+	cout<<"Timp = "<<elapsed_secs<<" secunde.\n";
 
-	// clean up pointers
+	cout<<"Terminat de inmultit numerele.\n";
+
+	// int16_t int_answer = bootsSymDecrypt(&result[0], key)*(int)pow(2, 2*N-1);
+	int16_t int_answer = 0;
+   for (int i=0; i<2*N; i++) {
+        int ai = bootsSymDecrypt(&result[i], key);
+        int_answer |= (ai<<(2*N-1-i));
+	cout<<ai;
+    }
+   cout<<endl;
+
+	/*for(int i=1; i<2*N; i++){
+		int_answer += bootsSymDecrypt(&result[i], key)*(int)pow(2, 2*N-1-i); 
+	}*/
+
+	cout<<"Produsul este "<<int_answer<<endl;
+
     delete_gate_bootstrapping_ciphertext(bit_product);
     delete_gate_bootstrapping_ciphertext(carry);
     delete_gate_bootstrapping_ciphertext(aux);
